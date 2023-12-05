947bf636d584a1e84b37e1e9211873d813ff183f
Tom Rideout <tom.rideout@betterhelp.com>
2023-12-05 15:00:06 -0800

    Models and database

---
+++ database/migrations/2023_12_05_224949_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('message_text');
+            $table->tinyInteger('analysis_status');
+            $table->timestamps();
+        });
+
+        Schema::create('analyses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('provider');
+            $table->text('raw_response');
+            $table->text('interpreted_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+        Schema::dropIfExists('analyses');
+    }
+};
