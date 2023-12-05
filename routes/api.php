8ada02736b4a487aee2ed0a851381ee3c394abcf
Chad French <chad.french@betterhelp.com>
2023-12-05 15:31:01 -0800

    testing

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes chad test
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
