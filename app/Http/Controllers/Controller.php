dd7ce0dc0c8ce08fca2e1e3a96fca9585a4dcdf3
Chad French <chad.french@betterhelp.com>
2023-12-05 23:02:45 -0800

    add full documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Second Opinion API",
+ *     version="1.0"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
