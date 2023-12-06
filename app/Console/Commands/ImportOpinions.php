3433579743e398a0685a540c3dc39dbfc00c7573
Tom Rideout <tom.rideout@betterhelp.com>
2023-12-05 19:10:06 -0800

    Importing of opinions csv

---
+++ app/Console/Commands/ImportOpinions.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Opinion;
+use Illuminate\Console\Command;
+
+class ImportOpinions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-opinions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import a CSV of pre-existing opinions with which to train the LLM';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (!file_exists(base_path('import.csv'))) {
+            $this->error('file "import.csv" does not exist');
+            return;
+        }
+        $imported = 0;
+        $file = fopen(base_path('import.csv'));
+        while ($row = fgetcsv($file)) {
+            Opinion::create([
+                'message' => $row[0],
+                'urgency' => $row[1],
+            ]);
+            $imported++;
+        }
+        $this->alert($imported . ' opinions imported');
+    }
+}
